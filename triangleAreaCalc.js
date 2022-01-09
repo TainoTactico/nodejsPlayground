d2992b7ed407e0b946ea5d860a01c4cb49b4e2a9
devramune <96541651+devramune@users.noreply.github.com>
2022-01-09 12:03:28 -0600

    Added triangle area calculator (triangleAreaCalc.js) to repository.

---
+++ triangleAreaCalc.js
@@ -0,0 +1,19 @@
+//variables
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+side = 0;
+
+readline.question('What is the length of one of the sides of the triangle? ', input => {
+    side = input;
+    //capture user input
+    //side = 8;
+
+    //calculate square area
+    area = side * side / 2;
+
+    //output area
+    console.log("Area of the triangle is: " + area);
+    readline.close();
+});
