684146b36f2767f112c27332569eb983f1e45064
devramune <96541651+devramune@users.noreply.github.com>
2022-01-12 19:45:29 -0600

    Added function areaCalc to areaCalculator.js. Update 0.1.1.

--- areaCalculator.js
+++ areaCalculator.js
@@ -1,8 +1,18 @@
 const { Command } = require('commander');
 const program = new Command();
 
+//Functions of the program.
+function calcArea(base, height, type){
+    if(type == "triangle"){
+        return ((base * height) / 2);
+    }
+    else if(type == "rectangle"){
+        return (base * height);
+    }
+    else {return ("undefined")}
+}
 program
-    .version('version: 0.1')
+    .version('version: 0.1.1')
 
 program
     .command('rectangle')
@@ -10,7 +20,7 @@ program
     .option('-b, --base <number>', 'Base of the rectangle')
     .option('-h, --height <number>', 'Height of the rectangle')
     .action((options) => {
-        area = (options.base * options.height);
+        area = calcArea(options.base, options.height, "rectangle");
         console.log('The area of the rectangle is: %s ', area);
     });
 
@@ -20,7 +30,7 @@ program
     .option('-b, --base <number>', 'Base of the triangle')
     .option('-h, --height <number>', 'Height of the triangle')
     .action((options) => {
-        area = (options.base * options.height) / 2;
+        area = calcArea(options.base, options.height, "triangle");
         console.log('The area of the triangle is: %s ', area);
     });
 
